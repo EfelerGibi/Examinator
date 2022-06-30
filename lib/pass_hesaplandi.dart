fceac7d4e31772d9247c7a6a974aa7f82c7f4a14
EfelerGibi <efecilginoyuncu@gmail.com>
2022-06-30 10:23:57 +0300

    Added the Calc Part

--- lib/pass_hesaplandi.dart
+++ lib/pass_hesaplandi.dart
@@ -4,15 +4,53 @@ import 'package:flutter/material.dart';
 class PassHesaplandi extends StatelessWidget {
   final String title;
   final Map controller;
+  final Map _dersler = {};
 
-  const PassHesaplandi({
+  PassHesaplandi({
     Key? key,
     required this.controller,
     required this.title,
   }) : super(key: key);
 
   String hesaplama() {
-    return "";
+    double ortalama = 0;
+    double kredi = 0;
+
+    _dersler[controller["name"]] = {
+      "final": (((100 * parseDouble(controller["gnotu"]) -
+              (parseDouble(controller["vizew"]) *
+                  multiOrtala(controller["vize"])) -
+              (parseDouble(controller["odevw"], r: 0.0) *
+                  multiOrtala(controller["odev"]))) /
+          parseDouble(controller["finalw"])))
+    };
+
+    // for (var ders in _dersler.keys) {
+    //   ortalama += _dersler[ders]["final"] * _dersler[ders]["kredi"];
+    //   kredi += _dersler[ders]["kredi"];
+    // }
+    return (_dersler[controller["name"]]["final"]).toStringAsFixed(2);
+  }
+
+  double parseDouble(value, {r = 1.0}) {
+    if (double.tryParse(value.text) != null) {
+      return double.parse(value.text);
+    }
+    return r;
+  }
+
+  double multiOrtala(Map value) {
+    double toplam = 0;
+    int sayi = 0;
+    for (var i in value.values) {
+      toplam += double.parse(i[1].text);
+      sayi++;
+    }
+    if (sayi != 0) {
+      return toplam / sayi;
+    } else {
+      return 0;
+    }
   }
 
   @override
